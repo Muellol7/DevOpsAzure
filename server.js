4293e45253345cac38f25bce04aa426bb8d5ba79
Muellol7 <muellol7@students.zhaw.ch>
2024-05-26 18:14:58 +0200

    Change mit GitHub

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 - Muellol7)');
+  res.send('Hello DevOps FS2024 - Muellol7 neue Änderung mit GitHub');
 });
 
 app.listen(PORT, HOST);
